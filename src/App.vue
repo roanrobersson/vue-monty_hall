8cdc78243416975db92f2c0f3215ca1885dd929c
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-08-28 00:36:06 -0300

    Change body css

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
+    <MontyHall/>
   </div>
 </template>
 
 <script>
+import MontyHall from './components/MontyHall.vue'
+
 export default {
   name: 'App',
+  components: {
+    MontyHall,
+  }
 }
 </script>
 
 <style>
+body {
+    margin: 0;
+    box-sizing: border-box;
+    image-rendering: optimizeSpeed;
+    font-family: 'silkscreennormal', Arial, sans-serif;
+    font-size: 16px;
+    background-color: lightBlue;
+    white-space: nowrap;
+}
+@media screen and (min-width: 600px) {
+    body {
+        font-size: 24px;
+    }
+}
 </style>
