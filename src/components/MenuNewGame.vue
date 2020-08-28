895f9c547a87cfa2da157c3bdf1066b69b5a56c5
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-08-28 00:34:35 -0300

    Add MenuNewGame component

---
+++ src/components/MenuNewGame.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="menu-newGame">
+        <div>
+            Doors: <span id="doorsCount">{{doorsQuantity}}</span>
+        </div>
+        <Slider 
+            :min='3'
+            :max='100'
+            @slide='doorsQuantity = $event'
+        />
+        <Button label='New Game' 
+            @click.stop.native="$emit('new-game', doorsQuantity)"
+        />
+    </div>
+</template>
+
+<script>
+import Button from './Button.vue'
+import Slider from './Slider.vue'
+
+export default {
+    name: 'MenuNewGame',
+
+    components: {
+        Button,
+        Slider,
+    },
+    
+    data: function () {
+        return {
+            doorsQuantity: 3,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.menu-newGame {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.menu-newGame > * {
+    margin-bottom: 0.5em;
+}
+</style>
