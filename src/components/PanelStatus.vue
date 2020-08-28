9b0a637aeae55e9ff4842a3f12140988cc55255d
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-08-28 00:34:19 -0300

    Add PanelStatus component

---
+++ src/components/PanelStatus.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="panelStatus">
+        <div class="panelStatus-item">
+            Wins: <span>{{wins}}</span>
+        </div>
+        <div class="panelStatus-item">
+            Loses: <span>{{loses}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PanelStatus',
+    
+    props: {
+            wins: {
+                    type: Number,
+                    required: true,
+                    default: 0,
+            },
+            loses: {
+                    type: Number,
+                    required: true,
+                    default: 0,
+            }
+    }
+}
+</script>
+
+<style scoped>
+.panelStatus > * {
+    margin-bottom: 0.5em;
+}
+.panelStatus-item {
+
+}
+</style>
