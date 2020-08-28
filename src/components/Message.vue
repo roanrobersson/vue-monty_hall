463926b728f6bb58866c2cdd11c4532ef7b26d4e
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-08-28 00:33:35 -0300

    Add Message component

---
+++ src/components/Message.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="message" v-if="text !== ''">
+        {{ text }} 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Message',
+    
+    props: {
+            text: {
+                    type: String,
+                    required: false,
+                    default: '',
+            },
+    }
+}
+</script>
+
+<style scoped>
+.message {
+    color: black;
+    border: solid white 2px;
+    padding: 5px 0;
+    text-align: center;
+}
+
+@media screen and (min-width: 600px) {
+    .message {
+        padding: 5px 70px;
+    }
+}
+</style>
